ea456e93662485c9f8e884a6a77b6ff6ba0b8a9f
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-25 23:41:44 +0800

    Program Instructions and Pseudocode

---
+++ print_the_sum_of_the_current_number_and_the_previous_number.py
@@ -0,0 +1,6 @@
+# Write a program to iterate the first 10 numbers, 
+# and in each iteration, print the sum of the current and previous number.
+
+# Set a starting variable of 0
+# Make iteration, range of 10
+# Print the sum of current and previous number
