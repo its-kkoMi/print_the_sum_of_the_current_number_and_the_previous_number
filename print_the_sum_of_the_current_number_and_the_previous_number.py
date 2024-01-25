fd0f01690486fdf418dbdbbeb2da41c4c111b4f6
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-25 23:48:25 +0800

    Added the iteration and printing of output

--- print_the_sum_of_the_current_number_and_the_previous_number.py
+++ print_the_sum_of_the_current_number_and_the_previous_number.py
@@ -2,5 +2,13 @@
 # and in each iteration, print the sum of the current and previous number.
 
 # Set a starting variable of 0
+
+previous_number = 0
+
 # Make iteration, range of 10
 # Print the sum of current and previous number
+
+for i in range(1, 11):
+    sum_of_current_and_previous = previous_number + i
+    print("Current Number", i, "Previous Number", previous_number, "Sum", sum_of_current_and_previous)
+    previous_number = i
